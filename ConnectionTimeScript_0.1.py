52706155285b8237bbc93700e5e1023f6eb328ee
danielScreenovate <danielbenzvi@screenovate.com>
2015-11-10 17:28:41 +0200

    Some improvements

--- ConnectionTimeScript_0.1.py
+++ ConnectionTimeScript_0.1.py
@@ -14,7 +14,6 @@ import subprocess
 
 # MADE FOR WINDOWS 10
 
-DEFAULT_MONITOR_NAME = "Dell 3a59"
 PRINTING_FRAME_LINE = "Entering function void screenovate::WfdDepacketizer::onNewVideoFrame"
 
 def get_connect_time_and_disconnect():
@@ -24,62 +23,95 @@ def get_connect_time_and_disconnect():
 
     total_connection_time = 0
     for x in range(number_of_connections):
+        print("Connection number %s" %x)
         total_connection_time += connect()
         disconnect()
 
     return "Number of connections: %s\n" \
            "Average connection time: %s" % (number_of_connections, total_connection_time / number_of_connections)
 
-def connect():
+
+#TODO: Implement in a more elegant way
+def is_connected():
+    print("Is the Source connected to the Monitor?")
+    is_connected = True
     root_element = pyuiautomation.GetRootElement()
-    autoit.send("#k")
-    time.sleep(3)
+    open_project_bar()
+    #Find 'Disconnect' button
+    button_disconnect = root_element.findfirst('descendants', Name='Disconnect')
+    if str(button_disconnect) == 'None':
+        is_connected = False
+        print("Nope")
+    else:
+        print("Yup")
+
+    go_to_desktop()
+
+    return is_connected
+
+def connect():
+
+    go_to_desktop()
+    if is_connected():
+        disconnect()
+    root_element = pyuiautomation.GetRootElement()
+    open_connect_bar()
 
     # Get monitor button:
     button_monitor = get_monitor_button(root_element)
     print("Connecting to monitor")
-    print(str(button_monitor))
     time.sleep(2)
     starting_time = time.time()
     button_monitor.Invoke()
     output = subprocess.check_output(["adb", "logcat", "|", "findstr", PRINTING_FRAME_LINE])
     ending_time = time.time()
-    print("output type is: %s" % type(output))
+    print("Successfully connected!")
+
+    go_to_desktop()
     return ending_time - starting_time
 
 def disconnect():
-    root_element = pyuiautomation.GetRootElement()
+    go_to_desktop()
+    print("Disconnecting")
+    if is_connected():
+        root_element = pyuiautomation.GetRootElement()
+        open_project_bar()
+        button_disconnect = root_element.findfirst('descendants', Name='Disconnect')
+
+        button_disconnect.Invoke()
+        time.sleep(2)
+    go_to_desktop()
+
+def open_connect_bar():
+    autoit.send("#k")
+    time.sleep(2)
+
+def open_project_bar():
     autoit.send("#p")
     time.sleep(2)
-    button_disconnect = root_element.findfirst('descendants', Name='Disconnect')
-    if str(button_disconnect) == 'None':
-        button_disconnect = root_element.findfirst('descendants', Name='PC screen only')
-        
-    button_disconnect.Invoke()
-    time.sleep(2)
+
+def go_to_desktop():
+    autoit.send("#D")
 
 def get_monitor_button(root_element):
     monitor_name = subprocess.check_output("adb shell getprop | findstr ssid", shell=True)
-    print("Full output: %s" % monitor_name)
     start_index = monitor_name.rfind('Dell', 0)
-    print(monitor_name[start_index:])
     monitor_name = monitor_name[start_index:]
     end_index = monitor_name.rfind(']',0)
-    print("end_index is: %s" % end_index)
-    print("Monitor name is: %s" % monitor_name[:end_index])
-
-    # monitor_name = DEFAULT_MONITOR_NAME
-    # button_monitor = root_element.findfirst('descendants', monitor_name)
-    # if button_monitor == None:
-    #     monitor_name = raw_input("Please insert Monitor name, for example: \nDell 5a74")
-    #     button_monitor = root_element.findfirst('descendants', monitor_name)
-    #
-    # while button_monitor == None:
-    #     print 'Monitor name not found'
-    #     monitor_name = raw_input("Please insert Monitor name, for example: \nDell 5a74")
+    monitor_name = monitor_name[:end_index]
+    print("Monitor name is: %s" % monitor_name)
     button_monitor = root_element.findfirst('descendants', Name=monitor_name)
-    print("Root element = %s" % str(root_element))
 
     return button_monitor
 
 print(get_connect_time_and_disconnect())
+
+
+
+# TODO: Implement alternate (and better) way of retrieving Monitor's name for connection.
+# Following rows are relevant:
+#
+# element = monitor.device(resourceId='com.screenovate.dell.monitorserver:id/video_surface_view')
+# Michael:
+# "search for id.ssid in process com.screenovate.dell.monitorserver (not in process com.screenovate.dell.monitorserver:id).
+# to get the monitor name"
