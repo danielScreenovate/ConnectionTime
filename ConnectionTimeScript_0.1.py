2c5070e5c1ae3db1b94a68d630e80a47b5950f4e
danielScreenovate <danielbenzvi@screenovate.com>
2015-11-11 09:52:44 +0200

    Longer waiting time, added output

--- ConnectionTimeScript_0.1.py
+++ ConnectionTimeScript_0.1.py
@@ -23,8 +23,10 @@ def get_connect_time_and_disconnect():
 
     total_connection_time = 0
     for x in range(number_of_connections):
-        print("Connection number %s" %x)
-        total_connection_time += connect()
+        print("Connection number %s" % x)
+        connection_time = connect()
+        print("Connection time: %s" % connection_time)
+        total_connection_time += connection_time
         disconnect()
 
     return "Number of connections: %s\n" \
@@ -50,8 +52,6 @@ def is_connected():
     return is_connected
 
 def connect():
-
-    go_to_desktop()
     if is_connected():
         disconnect()
     root_element = pyuiautomation.GetRootElement()
@@ -60,40 +60,41 @@ def connect():
     # Get monitor button:
     button_monitor = get_monitor_button(root_element)
     print("Connecting to monitor")
-    time.sleep(2)
+    time.sleep(3)
     starting_time = time.time()
     button_monitor.Invoke()
     output = subprocess.check_output(["adb", "logcat", "|", "findstr", PRINTING_FRAME_LINE])
     ending_time = time.time()
     print("Successfully connected!")
+    time.sleep(4)
 
     go_to_desktop()
     return ending_time - starting_time
 
 def disconnect():
-    go_to_desktop()
     print("Disconnecting")
-    if is_connected():
-        root_element = pyuiautomation.GetRootElement()
-        open_project_bar()
-        button_disconnect = root_element.findfirst('descendants', Name='Disconnect')
-
+    root_element = pyuiautomation.GetRootElement()
+    open_project_bar()
+    button_disconnect = root_element.findfirst('descendants', Name='Disconnect')
+    if str(button_disconnect) != 'None':
         button_disconnect.Invoke()
-        time.sleep(2)
+        time.sleep(3)
     go_to_desktop()
 
 def open_connect_bar():
     autoit.send("#k")
-    time.sleep(2)
+    time.sleep(3)
 
 def open_project_bar():
     autoit.send("#p")
-    time.sleep(2)
+    time.sleep(3)
 
 def go_to_desktop():
-    autoit.send("#D")
+    autoit.send("#d")
+    time.sleep(3)
 
 def get_monitor_button(root_element):
+    print("retrieveing monitor button")
     monitor_name = subprocess.check_output("adb shell getprop | findstr ssid", shell=True)
     start_index = monitor_name.rfind('Dell', 0)
     monitor_name = monitor_name[start_index:]
