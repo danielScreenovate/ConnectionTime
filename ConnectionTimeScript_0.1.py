e375a46b0bc7712ea4c6ee1cad75fd4cb7b3f175
danielScreenovate <danielbenzvi@screenovate.com>
2015-11-12 10:32:29 +0200

    Writing to .csv file

--- ConnectionTimeScript_0.1.py
+++ ConnectionTimeScript_0.1.py
@@ -11,23 +11,29 @@ import autoit
 import os
 import time
 import subprocess
+import csv
 
 # MADE FOR WINDOWS 10
 
 PRINTING_FRAME_LINE = "Entering function void screenovate::WfdDepacketizer::onNewVideoFrame"
 
 def get_connect_time_and_disconnect():
+    connection_times = []
     number_of_connections = input("Enter number of connections: ")
     while (not isinstance(number_of_connections, int)) or number_of_connections <= 0:
         number_of_connections = input("Please enter an integer larger than 0:")
 
     total_connection_time = 0
     for x in range(number_of_connections):
-        print("Connection number %s" % x)
+        # print("Connection number %s" % (x + 1))
         connection_time = connect()
-        print("Connection time: %s" % connection_time)
+        connection_times.append(connection_time)
+        # print("Connection time: %s" % connection_time)
         total_connection_time += connection_time
         disconnect()
+
+    # Write connection times to .csv file
+    write_to_csv(connection_times)
 
     return "Number of connections: %s\n" \
            "Average connection time: %s" % (number_of_connections, total_connection_time / number_of_connections)
@@ -35,7 +41,7 @@ def get_connect_time_and_disconnect():
 
 #TODO: Implement in a more elegant way
 def is_connected():
-    print("Is the Source connected to the Monitor?")
+    # print("Is the Source connected to the Monitor?")
     is_connected = True
     root_element = pyuiautomation.GetRootElement()
     open_project_bar()
@@ -59,20 +65,20 @@ def connect():
 
     # Get monitor button:
     button_monitor = get_monitor_button(root_element)
-    print("Connecting to monitor")
+    # print("Connecting to monitor")
     time.sleep(3)
     starting_time = time.time()
     button_monitor.Invoke()
     output = subprocess.check_output(["adb", "logcat", "|", "findstr", PRINTING_FRAME_LINE])
     ending_time = time.time()
-    print("Successfully connected!")
+    # print("Successfully connected!")
     time.sleep(4)
 
     go_to_desktop()
     return ending_time - starting_time
 
 def disconnect():
-    print("Disconnecting")
+    # print("Disconnecting")
     root_element = pyuiautomation.GetRootElement()
     open_project_bar()
     button_disconnect = root_element.findfirst('descendants', Name='Disconnect')
@@ -105,4 +111,18 @@ def get_monitor_button(root_element):
 
     return button_monitor
 
+def write_to_csv(connection_times):
+    curr_time = time.time()
+    directory = "test_results"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    with open('%s/test_%s.csv' % (directory, curr_time), 'w+') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for x in range(len(connection_times)):
+            csv_writer.writerow((x + 1, connection_times[x]))
+
+
 print(get_connect_time_and_disconnect())
+autoit.send("#d")
+ctypes.windll.user32.MessageBoxA(0, "Finished running connection time measurement script.", "Testing Complete!", 1)
